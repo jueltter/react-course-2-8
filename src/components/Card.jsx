7cc943fa87483ed8d1aa94ed05778636682835e4
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-03-15 02:56:16 -0500

    styles inline 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Card = () => {
+    const styles = {
+        border: '1px solid black', padding: '10px', background: '#00ff00'
+            }
+        
+            
+
     return (
-        <div style={{ border: '1px solid black', padding: '10px', background: '#ff0000' }}>
+        <div style={styles}>
             This is a card!
         </div>
     );
